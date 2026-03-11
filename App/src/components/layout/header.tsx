89d38471855fe9de5ea296537d8d47f4e2699f73
Bela <belacaste3@gmail.com>
2026-03-11 12:17:39 -0500

    Adding header structure and assets

---
+++ App/src/components/layout/header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../../assets/logo-ctx-final.png";
+import "./header.css";
+import { IoMdHelpCircle } from "react-icons/io";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header__brand">
+          <div className="header__logo">
+            <img src={logo} alt="Logo" />
+          </div>
+        </div>
+
+        <div className="header__module">
+          <h1>ESTIMADOR DE PERDIDA ESPERADA</h1>
+          <p>Modulo de Riesgo Crediticio</p>
+        </div>
+
+        <div className="header__icons">
+            <IoMdHelpCircle />
+        </div>
+      </header>
+
+      <hr />
+    </>
+  );
+}
