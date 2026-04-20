c9a583d51a2bac299cb517a86eee5759dd82996e
JuanCamiloMB <juancamilo.manjarres@gmail.com>
2026-04-20 11:17:41 -0500

    Doc: Instrucciones para build

--- App/src/components/ui/file_queue.tsx
+++ App/src/components/ui/file_queue.tsx
@@ -1,12 +1,29 @@
 import { useState, useEffect } from "react";
 import "./file_queue.css";
+import { LogEntryData } from "./register_console";
 
-export default function FileQueue() {
-
+export default function FileQueue({ logs }: { logs: LogEntryData[] }) {
+  const [lastMessage, setLastMessage] = useState<LogEntryData | null>(null);
+  useEffect(() => {
+    if (logs.length > 0) {
+      //update the text with last success or error log message
+      const lastLog = logs[logs.length - 1];
+      if (lastLog?.level === "SUCCESS" || lastLog?.level === "ERROR") {
+        setLastMessage(lastLog);
+      }
+    }
+  }, [logs]);
   return (
     <div className="cola-card">
-      <div className="cola-label">FILAS EN COLA</div>
-      <div className="cola-number">14</div>
+      {logs.length === 0 ? (
+        <div className="nothing-label">Aun no hay resultados</div>
+      ) : (
+        <div
+          className={`${lastMessage?.level === "SUCCESS" ? "success-label" : "error-label"}`}
+        >
+          {lastMessage?.message}
+        </div>
+      )}
     </div>
   );
 }
